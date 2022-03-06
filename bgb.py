6383378e8c5e797c8704d1f8d6c239377b5a7c89
YusufNoMark <100463228+YusufNoMark@users.noreply.github.com>
2022-03-06 17:37:48 +0300

    Update bgb.py

--- bgb.py
+++ bgb.py
@@ -50,6 +50,7 @@ client = commands.Bot(command_prefix=botp, intents=intents)
 async def on_ready():
     await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="Bilgisayar Günlükleri"))
     print("OK")
+    status_task.start()
 
     #🔒┇kayıt
     #🚪┇gelen-giden
